bd975cedd3d3aee02f889d46ba5571629de23257
Ronnzxc <103023215+Ronnzxc@users.noreply.github.com>
2023-08-02 05:08:20 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,55 +1,20 @@
  // Create Web server
  
-var express = require('express');
-var app = express();
-var fs = require('fs');
-
-// Create Web server
-var app = express();
-
-// Set up handlebars view engine
-var handlebars = require('express3-handlebars')
-   .create({ defaultLayout:'main' });
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
-
-// Set up body-parser
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// Set up public directory
-app.use(express.static(__dirname + '/public'));
-
-// Set up port
-app.set('port', process.env.PORT || 3000);
-
-// Set up database
-var mongoose = require('mongoose');
-var opts = {
-   server: {
-      socketOptions: { keepAlive: 1 }
-   }
-};
-switch(app.get('env')) {
-   case 'development':
-      mongoose.connect(credentials.mongo.development.connectionString, opts);
-      break;
-   case 'production':
-      mongoose.connect(credentials.mongo.production.connectionString, opts);
-      break;
-   default:
-      throw new Error('Unknown execution environment: ' + app.get('env'));
-}
-
-// Set up database schema
-var Comment = require('./models/comment.js');
-
-// Set up database schema
-var User = require('./models/user.js');
-
-// Set up database schema
-var Post = require('./models/post.js');
-
-// Set up database schema
-var Comment = req
+ // Import the express module
+ const express = require('express');
+ 
+ // Create a new web server
+ const app = express();
+ 
+ // Import the comments module
+ const comments = require('./comments');
+ 
+ // Add a route to the server
+ app.get('/comments', (req, res) => {
+   res.send(comments);
+ });
+ 
+ // Start listening on port 3000
+ app.listen(3000, () => {
+   console.log('Listening on port 3000');
+ });
