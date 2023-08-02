a80e69ba53d39f445559fce9b6bd28022d1f53f2
Ronnzxc <103023215+Ronnzxc@users.noreply.github.com>
2023-08-02 05:18:09 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,3 +1,84 @@
-
- // hello world
- // h
+ // Create web server
+ const express = require('express');
+ const app = express();
+ const port = 3000;
+ 
+ // Create routes
+ app.get('/', (req, res) => {
+   res.send('Hello World!');
+ });
+ 
+ // Start web server
+ app.listen(port, () => {
+   console.log(`Example app listening at http://localhost:${port}`);
+ });
+ 
+ // Create routes
+ app.get('/comments', (req, res) => {
+   res.send('Hello World!');
+ });
+ 
+ // Start web server
+ app.listen(port, () => {
+   console.log(`Example app listening at http://localhost:${port}`);
+ });
+ 
+ // Create routes
+ app.get('/comments', (req, res) => {
+   res.send('Hello World!');
+ });
+ 
+ // Start web server
+ app.listen(port, () => {
+   console.log(`Example app listening at http://localhost:${port}`);
+ });
+ 
+ // Create routes
+ app.get('/comments', (req, res) => {
+   res.send('Hello World!');
+ });
+ 
+ // Start web server
+ app.listen(port, () => {
+   console.log(`Example app listening at http://localhost:${port}`);
+ });
+ 
+ // Create routes
+ app.get('/comments', (req, res) => {
+   res.send('Hello World!');
+ });
+ 
+ // Start web server
+ app.listen(port, () => {
+   console.log(`Example app listening at http://localhost:${port}`);
+ });
+ 
+ // Create routes
+ app.get('/comments', (req, res) => {
+   res.send('Hello World!');
+ });
+ 
+ // Start web server
+ app.listen(port, () => {
+   console.log(`Example app listening at http://localhost:${port}`);
+ });
+ 
+ // Create routes
+ app.get('/comments', (req, res) => {
+   res.send('Hello World!');
+ });
+ 
+ // Start web server
+ app.listen(port, () => {
+   console.log(`Example app listening at http://localhost:${port}`);
+ });
+ 
+ // Create routes
+ app.get('/comments', (req, res) => {
+   res.send('Hello World!');
+ });
+ 
+ // Start web server
+ app.listen(port, () => {
+   console.log(`Example app listening at http://localhost:${port}`);
+ });
