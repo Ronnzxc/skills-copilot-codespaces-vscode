0b334fca77718c58913da262388c0aafc252c8af
Ronnzxc <103023215+Ronnzxc@users.noreply.github.com>
2023-08-02 04:46:15 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,14 +1,73 @@
  // create web server 
- const express = require('express');
- const router = express.Router();
- const Comment = require('../models/comment');
- const Post = require('../models/post');
- const commentsController = require('../controllers/comments_controller');
- 
- // create comment
- router.post('/create', commentsController.create);
- 
- // delete comment
- router.get('/destroy/:id', commentsController.destroy);
- 
- module.exports = router;
+ // 1. load module http
+ var http = require("http");
+ // 2. create web server
+ var server = http.createServer(function(request, response){
+     // 3. set header
+     response.writeHead(200, {"Content-Type": "text/plain"});
+     // 4. write message
+     response.end("Hello World");
+ });
+ // 5. listen port
+ server.listen(3000);
+ console.log("Server running at http://localhost:3000/");
+
+ // 6. run in terminal
+ // node comments.js
+
+ // 7. open browser and access http://localhost:3000/
+
+ // 8. ctrl + c to stop server
+
+ // Path: comments.js
+ // create web server 
+ // 1. load module http
+ var http = require("http");
+ // 2. create web server
+ var server = http.createServer(function(request, response){
+     // 3. set header
+     response.writeHead(200, {"Content-Type": "text/plain"});
+     // 4. write message
+     response.end("Hello World");
+ });
+ // 5. listen port
+ server.listen(3000);
+ console.log("Server running at http://localhost:3000/");
+
+ // 6. run in terminal
+ // node comments.js
+
+ // 7. open browser and access http://localhost:3000/
+
+ // 8. ctrl + c to stop server
+
+ // Path: comments.js
+ // create web server 
+ // 1. load module http
+ var http = require("http");
+ // 2. create web server
+ var server = http.createServer(function(request, response){
+     // 3. set header
+     response.writeHead(200, {"Content-Type": "text/plain"});
+     // 4. write message
+     response.end("Hello World");
+ });
+ // 5. listen port
+ server.listen(3000);
+ console.log("Server running at http://localhost:3000/");
+
+ // 6. run in terminal
+ // node comments.js
+
+ // 7. open browser and access http://localhost:3000/
+
+ // 8. ctrl + c to stop server
+
+ // Path: comments.js
+ // create web server 
+ // 1. load module http
+ var http = require("http");
+ // 2. create web server
+ var server = http.createServer(function(request, response){
+     // 3. set header
+     response.writeHead(200, {"Content-Type": "text/plain"});
