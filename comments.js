d449f4868e8f2e02ef88a0954f7d149dc921d04b
Ronnzxc <103023215+Ronnzxc@users.noreply.github.com>
2023-08-02 04:58:37 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,73 +1,64 @@
- // create web server 
- // 1. load module http
- var http = require("http");
- // 2. create web server
- var server = http.createServer(function(request, response){
-     // 3. set header
-     response.writeHead(200, {"Content-Type": "text/plain"});
-     // 4. write message
-     response.end("Hello World");
+ // Create Web server application with Express
+ const express = require('express');
+ const bodyParser = require('body-parser');
+ const app = express();
+
+ // Use body-parser middleware to parse HTTP message body
+ app.use(bodyParser.urlencoded({ extended: false }));
+ app.use(bodyParser.json());
+
+ // Define HTTP GET method to get all comments
+ app.get('/comments', (req, res) => {
+     res.json(comments);
  });
- // 5. listen port
- server.listen(3000);
- console.log("Server running at http://localhost:3000/");
 
- // 6. run in terminal
- // node comments.js
-
- // 7. open browser and access http://localhost:3000/
-
- // 8. ctrl + c to stop server
-
- // Path: comments.js
- // create web server 
- // 1. load module http
- var http = require("http");
- // 2. create web server
- var server = http.createServer(function(request, response){
-     // 3. set header
-     response.writeHead(200, {"Content-Type": "text/plain"});
-     // 4. write message
-     response.end("Hello World");
+ // Define HTTP POST method to add a new comment
+ app.post('/comments', (req, res) => {
+     const comment = {
+         id: comments.length + 1,
+         timestamp: Date.now(),
+         body: req.body.body,
+     };
+     comments.push(comment);
+     res.json(comment);
  });
- // 5. listen port
- server.listen(3000);
- console.log("Server running at http://localhost:3000/");
 
- // 6. run in terminal
- // node comments.js
-
- // 7. open browser and access http://localhost:3000/
-
- // 8. ctrl + c to stop server
-
- // Path: comments.js
- // create web server 
- // 1. load module http
- var http = require("http");
- // 2. create web server
- var server = http.createServer(function(request, response){
-     // 3. set header
-     response.writeHead(200, {"Content-Type": "text/plain"});
-     // 4. write message
-     response.end("Hello World");
+ // Define HTTP PUT method to update an existing comment
+ app.put('/comments/:id', (req, res) => {
+     const id = parseInt(req.params.id, 10);
+     const comment = comments.filter(comment => comment.id === id)[0];
+     comment.body = req.body.body;
+     res.json(comment);
  });
- // 5. listen port
- server.listen(3000);
- console.log("Server running at http://localhost:3000/");
 
- // 6. run in terminal
- // node comments.js
+ // Define HTTP DELETE method to delete an existing comment
+ app.delete('/comments/:id', (req, res) => {
+     const id = parseInt(req.params.id, 10);
+     const index = comments.findIndex(comment => comment.id === id);
+     comments.splice(index, 1);
+     res.status(204).send();
+ });
 
- // 7. open browser and access http://localhost:3000/
+ // Start the server and listen on port 3000
+ app.listen(3000, () => {
+     console.log('Server is running on port 3000');
+ });
 
- // 8. ctrl + c to stop server
-
- // Path: comments.js
- // create web server 
- // 1. load module http
- var http = require("http");
- // 2. create web server
- var server = http.createServer(function(request, response){
-     // 3. set header
-     response.writeHead(200, {"Content-Type": "text/plain"})});
+ // Define an array to store comments
+ const comments = [
+     {
+         id: 1,
+         timestamp: Date.now(),
+         body: 'First comment',
+     },
+     {
+         id: 2,
+         timestamp: Date.now(),
+         body: 'Second comment',
+     },
+     {
+         id: 3,
+         timestamp: Date.now(),
+         body: 'Third comment',
+     },
+ ];
